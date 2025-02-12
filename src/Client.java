9658ce8b78f087445122be6c197d2d5aa7c8992d
c-dager <dagerc@protonmail.com>
2025-02-12 10:18:12 -0500

    Add downloadFile function

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,5 @@
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
@@ -14,15 +15,45 @@ public class Client {
 
         int serverPort = Integer.parseInt(args[1]);
         Scanner scanner = new Scanner(System.in);
-        String fileName = scanner.nextLine();
-
+        System.out.println("Type the action you'd like to take: List, Delete, Rename, Download, or Upload: ");
+        String action = scanner.nextLine().toUpperCase();
         SocketChannel channel = SocketChannel.open();
         channel.connect(new InetSocketAddress(args[0], serverPort));
+
+        switch(action){
+            case "LIST":
+                listFiles();
+                break;
+            case "DELETE":
+                deleteFile();
+                break;
+            case "RENAME":
+                renameFiles();
+                break;
+            case "DOWNLOAD":
+                downloadFile(channel, scanner);
+                break;
+            case "UPLOAD":
+                uploadFile();
+                break;
+        }
+
+        channel.close();
+
+
+    }
+
+    private static void uploadFile() {
+    }
+
+    private static void downloadFile(SocketChannel channel, Scanner scanner) throws IOException {
+        System.out.println("Enter the file name to be downloaded");
+        String fileName =  scanner.nextLine();
+        String request = "DOWNLOAD " + fileName;
         ByteBuffer byteBuffer = ByteBuffer.wrap(fileName.getBytes());
         channel.write(byteBuffer);
         //request to close channel from client to server direction
         channel.shutdownOutput();
-
         FileOutputStream fileOutputStream = new FileOutputStream("ClientFiles/" + fileName, true);
         FileChannel fileChannel = fileOutputStream.getChannel();
         ByteBuffer fileContent = ByteBuffer.allocate(1024);
@@ -33,8 +64,15 @@ public class Client {
             fileContent.clear();
         }
         fileOutputStream.close();
-        channel.close();
+    }
 
+    private static void renameFiles() {
+    }
+
+    private static void deleteFile() {
+    }
+
+    private static void listFiles(){
 
     }
 
