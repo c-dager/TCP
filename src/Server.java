d2f08ea2c18ef72351448adbcb6bb3516aaa4fd0
c-dager <dagerc@protonmail.com>
2025-02-13 16:36:06 -0500

    Add uploadFileToClient

--- src/Server.java
+++ src/Server.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
@@ -17,29 +18,71 @@ public class Server {
             buffer.flip();
             byte[] a = new byte[bytesRead];
             buffer.get(a);
-            String fileName = new String(a);
-            System.out.println("File name message: " + fileName);
+            String clientRequest = new String(a);
+            String[] clientRequestList = clientRequest.split("\\|");
+            String action = clientRequestList[0];
 
-            File file = new File("ServerFiles/" + fileName);
-            if(!file.exists()){
-                System.out.println("File doesn't exist");
+            switch(action){
+                case "LIST":
+                    listFiles();
+                    break;
+                case "DELETE":
+                    deleteFile();
+                    break;
+                case "RENAME":
+                    renameFile();
+                    break;
+                case "DOWNLOAD":
+                    uploadFileToClient(serveChannel, clientRequestList[1]);
+                    break;
+                case "UPLOAD":
+                    downloadFileFromClient();
+                    break;
+                default:
+                    System.out.println("Invalid command, try again");
             }
-            else{
-                FileInputStream fileInputStream = new FileInputStream(file);
-                FileChannel fileChannel = fileInputStream.getChannel();
-                ByteBuffer fileContent = ByteBuffer.allocate(1024);
+            //serveChannel.close();
+        }
+    }
 
-                int byteRead = 0;
-                do {
-                    byteRead = fileChannel.read(fileContent);
-                    fileContent.flip();
-                    serveChannel.write(fileContent);
-                    fileContent.clear();
-                }while (byteRead >=0);
+    private static void downloadFileFromClient() {
+    }
 
-                fileInputStream.close();
-            }
+    private static void renameFile() {
+
+    }
+
+    private static void deleteFile() {
+
+    }
+
+    private static void listFiles() {
+
+    }
+
+    private static void uploadFileToClient(SocketChannel serveChannel, String fileName) throws IOException {
+        System.out.println("File name message: " + fileName);
+
+        File file = new File("ServerFiles/" + fileName);
+        if(!file.exists()){
+            System.out.println("File doesn't exist");
+        }
+        else{
+            FileInputStream fileInputStream = new FileInputStream(file);
+            FileChannel fileChannel = fileInputStream.getChannel();
+            ByteBuffer fileContent = ByteBuffer.allocate(1024);
+
+            int byteRead = 0;
+            do {
+                byteRead = fileChannel.read(fileContent);
+                fileContent.flip();
+                serveChannel.write(fileContent);
+                fileContent.clear();
+            }while (byteRead >=0);
+
+            fileInputStream.close();
             serveChannel.close();
+
         }
     }
 }
